6d567b8e512c52ad95b641f70b79da2427b3b457
lucaskhliu <liukehong@gmail.com>
2023-05-11 22:45:56 +0800

    add todos

---
+++ lua/server/config.js
@@ -0,0 +1,15 @@
+const user_home = "/Users/lucas";
+const config = {
+  home: `${user_home}/mdnotes`,
+  templates: `${user_home}/mdnotes/templates`,
+  smp_markdown_css: `${user_home}/.config/smp/my_markdown.css`,
+  smp_snippets_folder: `${user_home}/.config/smp/snippets`,
+  copy_file_into_assets: true,
+  show_indicator: true,
+  auto_preview: true,
+  show_navigation_panel: true,
+  show_navigation_content: true,
+  navigation_content_at: "right",
+}
+
+module.exports = config;
